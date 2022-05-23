3957280427de42cbaadb987742345e7ab1f36a41
Tim Slager <timtendo12@gmail.com>
2022-05-23 15:10:11 +0200

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const twitter = require("twitter");
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+    clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+    redirectUri: 'http://www.example.com/callback'
+});
+spotifyApi.setAccessToken('<your_access_token>');
